25f65f35d638e1b46f9c2f82587d593fc7abbf28
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-16 14:04:59 +0600

    Added functionality
    Play Pause

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
     $('.menu-btn').click(function() {
         $('.mobile-menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    $('#play-pause').click(function() {
+        if($('#header-video').get(0).paused) {
+            $('#header-video').get(0).play();
+            $('#play-pause').html("Pause");
+        } else {
+            $('#header-video').get(0).pause();
+            $('#play-pause').html("Play");
+        }
     })
 });
 
